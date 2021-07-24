a6b32663233e92aae069003f5181a20f0cbc10f6
Ryan Prichard <rprichard@google.com>
2021-07-23 17:52:49 -0700

    Switch to repo-style checkout paths
    Make kokoro_build.cmd and kokoro_build.sh work on a local machine where
    the Kokoro variables haven't been set. Use "dev" as the build ID in
    this case.
    
    Switch to Visual Studio 2017, which doesn't set an environment variable
    like VS140COMNTOOLS, so search for it at the default install path.
    
    Switch to a prebuilt Python so it's easier to build it on a Windows
    machine. e.g. My laptop has a "C:\Program Files\Python39" directory but
    no "C:\python37".
    
    Change-Id: I368d4f281820e7bc6c77ce5f64099d68000fe278

--- kokoro/kokoro_build.ps1
+++ kokoro/kokoro_build.ps1
@@ -1,9 +1,14 @@
-$DEST      = "$ENV:KOKORO_ARTIFACTS_DIR\dest"
-$OUT       = "$ENV:KOKORO_ARTIFACTS_DIR\out"
-$CMAKE_SRC = "$ENV:KOKORO_ARTIFACTS_DIR\git\cmake_src"
-$PYTHON    = "C:\python37\python"
+$top       = (Resolve-Path "$PSScriptRoot\..\..\..")
+$out       = "$top\out\cmake"
+$cmake_src = "$top\external\cmake"
+$python    = "$top\prebuilts\python\windows-x86\python.exe"
 
-pushd "$ENV:VS140COMNTOOLS..\..\VC"
+Remove-Item $out -Recurse -ErrorAction Ignore
+
+$vcvarsall = (Resolve-Path "C:\Program Files*\Microsoft Visual Studio\2017\*\VC\Auxiliary\Build\vcvarsall.bat")[-1]
+echo "Invoking $vcvarsall to configure clang-cl"
+
+pushd (Split-Path $vcvarsall)
 cmd /c "vcvarsall.bat amd64 & set" |
 foreach {
   if ($_ -match "=") {
@@ -16,18 +21,19 @@ popd
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
 $toolkit_path = (Resolve-Path "C:\Program Files*\Windows Kits\10\bin\*\x64\rc.exe")[-1] | Split-Path
+echo "Adding $toolkit_path to end of PATH"
 $ENV:PATH += ";$toolkit_path"
 
 # Remove cygwin from path so that cmake will not detect its compiler.
 $ENV:PATH = ($ENV:PATH.Split(';') | Where-Object { $_ -notmatch 'cygwin' }) -join ';'
 
-& $PYTHON --version
-if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+$build_id = $ENV:KOKORO_BUILD_ID
+$build_id = if ($build_id -eq $null) { "dev" } else { $build_id }
 
-& $PYTHON "$PSScriptRoot\build.py", $CMAKE_SRC, $OUT, $DEST, $ENV:KOKORO_BUILD_ID,
-  "--cmake=$ENV:KOKORO_ARTIFACTS_DIR\git\cmake\bin\cmake.exe",
-  "--ninja=$ENV:KOKORO_ARTIFACTS_DIR\git\ninja\ninja.exe",
-  "--android-cmake=$ENV:KOKORO_ARTIFACTS_DIR\git\android-cmake"
-  "--clang-repo=$ENV:KOKORO_ARTIFACTS_DIR\git\clang"
+& $python "$PSScriptRoot\build.py", $cmake_src, $out, "$out\artifact", $build_id,
+  "--cmake=$top\prebuilts\cmake\windows-x86\bin\cmake.exe",
+  "--ninja=$top\prebuilts\ninja\windows-x86\ninja.exe",
+  "--android-cmake=$top\external\android-cmake",
+  "--clang-repo=$top\prebuilts\clang\host\windows-x86"
 
 exit $LASTEXITCODE
--- kokoro/kokoro_build.sh
+++ kokoro/kokoro_build.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
-
-# Fail on any error.
 set -e
 
-DEST="${KOKORO_ARTIFACTS_DIR}/dest"
-OUT="${KOKORO_ARTIFACTS_DIR}/out"
-CMAKE_SRC=${KOKORO_ARTIFACTS_DIR}/git/cmake_src
+top=$(cd $(dirname $0)/../../.. && pwd)
 
-BASEDIR=$(dirname "$0")
+out=$top/out/cmake
+cmake_src=$top/external/cmake
 
-python3 --version
-python3 ${BASEDIR}/build.py "${CMAKE_SRC}" "${OUT}" "${DEST}" "${KOKORO_BUILD_ID}" \
-  --cmake=${KOKORO_ARTIFACTS_DIR}/git/cmake/bin/cmake \
-  --ninja=${KOKORO_ARTIFACTS_DIR}/git/ninja/ninja \
-  --android-cmake=${KOKORO_ARTIFACTS_DIR}/git/android-cmake \
-  --clang-repo=${KOKORO_ARTIFACTS_DIR}/git/clang
+case $(uname -s) in
+  Linux) host=linux-x86 ;;
+  Darwin) host=darwin-x86 ;;
+  *) echo "Unrecognized uname -s: $(uname -s)"; exit 1 ;;
+esac
+
+rm -fr $out
+
+$top/prebuilts/python/$host/bin/python3 \
+  $cmake_src/kokoro/build.py $cmake_src $out $out/artifact "${KOKORO_BUILD_ID:-dev}" \
+  --cmake=$top/prebuilts/cmake/$host/bin/cmake \
+  --ninja=$top/prebuilts/ninja/$host/ninja \
+  --android-cmake=$top/external/android-cmake \
+  --clang-repo=$top/prebuilts/clang/host/$host
